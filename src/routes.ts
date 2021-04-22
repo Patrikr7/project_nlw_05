82da0e4be9c3b82c136149f91ceafee0827b6ecf
Emerson Patrik <epribeiro@clig.net.br>
2021-04-22 20:21:56 -0300

    NLW 05 - Aula 03

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
+const usersController = new UsersController();
 const settingsController = new SettingsController();
+const messagesController = new MessagesController();
 
+routes.post("/users", usersController.create);
 routes.post("/settings", settingsController.create);
+
+routes.get("/messages/:id", messagesController.showByUser);
+routes.post("/messages", messagesController.create);
 
 export { routes };
---
+++ src/services/UsersService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import { User } from "../entities/User";
+
+class UserService {
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string) {
+        // Verifica se o usuário existe
+        const userExists = await this.usersRepository.findOne({ email });
+        
+        // Se existir, retornar o usuário
+        if (userExists) {
+            return userExists;
+        }
+        
+        const user = this.usersRepository.create({ email });
+
+        await this.usersRepository.save(user);
+        
+        // Se não existir, salvar no DB.
+        return user;
+    }
+}
+
+export { UserService }
